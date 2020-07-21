34c8367835ae49408097af4febfc8e2a95934759
Maysoon Tabali <mtab03@me.com>
2020-07-21 19:37:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,6 +17,7 @@ def literal_aoh
     {name:  "Don Gately", occupation: "Live-in Staff"}, 
     {name:  "Joelle van Dyne", occupation: "Radio Personality"}, 
     {name:  "Pat Monteseian", occupation: "Staff"}, 
+    
     {name:  "Bruce Green", occupation: "Fan of Mildred"}
     ]
     literal_aoh
